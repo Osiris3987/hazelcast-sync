3e67a0710eeefdd721c368dbcf07c07c63992834
Oleg Lomakin <yfnf72@gmail.com>
2024-04-13 15:50:24 +0300

    initialized project

---
+++ src/main/java/com/example/hackathon_becoder_backend/HackathonBecoderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackathon_becoder_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonBecoderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonBecoderBackendApplication.class, args);
+    }
+
+}
