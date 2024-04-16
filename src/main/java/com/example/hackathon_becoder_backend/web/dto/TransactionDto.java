7f76a9139c5eff0304f1e622f10c3149c726b3a4
pavelmalkov <pashamalkovich@mail.ru>
2024-04-16 23:10:26 +0300

    TransactionDTO constraints added

--- src/main/java/com/example/hackathon_becoder_backend/web/dto/TransactionDto.java
+++ src/main/java/com/example/hackathon_becoder_backend/web/dto/TransactionDto.java
@@ -1,6 +1,9 @@
 package com.example.hackathon_becoder_backend.web.dto;
 
 import com.example.hackathon_becoder_backend.domain.transaction.TransactionType;
+import com.example.hackathon_becoder_backend.web.dto.validation.OnCreate;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -8,12 +11,14 @@ import java.util.UUID;
 
 @Data
 public class TransactionDto {
-    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    @Null(groups = OnCreate.class)
     private UUID id;
+    @NotNull(groups = OnCreate.class)
     private TransactionType type;
+    @NotNull(groups = OnCreate.class)
     private BigDecimal amount;
-    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    @NotNull(groups = OnCreate.class)
     private UUID clientId;
-    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    @NotNull(groups = OnCreate.class)
     private UUID legalEntityId;
 }
---
+++ src/main/java/com/example/hackathon_becoder_backend/web/dto/validation/OnCreate.java
@@ -0,0 +1,4 @@
+package com.example.hackathon_becoder_backend.web.dto.validation;
+
+public @interface OnCreate {
+}
