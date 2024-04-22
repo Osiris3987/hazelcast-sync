bf43545c1fec9d5a6df1c4bb4484dbde94473bbf
Zerumi <afanasyev09032005@gmail.com>
2024-04-23 01:15:00 +0300

    feat: implement LegalEntityMapper

--- src/main/java/com/example/hackathon_becoder_backend/web/dto/LegalEntityDto.java
+++ src/main/java/com/example/hackathon_becoder_backend/web/dto/LegalEntityDto.java
@@ -1,7 +1,16 @@
 package com.example.hackathon_becoder_backend.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 @Data
 public class LegalEntityDto {
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    private UUID id;
+    private String name;
+    private BigDecimal balance;
+    private Integer version;
 }
--- src/main/java/com/example/hackathon_becoder_backend/web/mapper/LegalEntityMapper.java
+++ src/main/java/com/example/hackathon_becoder_backend/web/mapper/LegalEntityMapper.java
@@ -1,8 +1,14 @@
 package com.example.hackathon_becoder_backend.web.mapper;
 
+import com.example.hackathon_becoder_backend.domain.legal_entity.LegalEntity;
+import com.example.hackathon_becoder_backend.web.dto.LegalEntityDto;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring", uses = ClientMapper.class)
 public interface LegalEntityMapper {
-
+    LegalEntityDto toDto(LegalEntity legalEntity);
+    LegalEntity toEntity(LegalEntityDto legalEntityDto);
+    List<LegalEntityDto> toDtoList(List<LegalEntity> legalEntityList);
 }
